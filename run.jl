93516d89375ea5813bcf8cfb25cbdccfa1ed04f1
Alec Hoyland <alec.hoyland@posteo.net>
2019-11-23 15:37:01 -0500

    Remove whitespace

--- run.jl
+++ run.jl
@@ -3,41 +3,26 @@ using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
 
+@info "including conv"
+include("conv.jl")
 
-    @info "including conv"
-    include("conv.jl")
+@info "including convFuzz"
+include("convFuzz.jl")
 
+@info "including convAdpt"
+include("convAdpt.jl")
 
+@info "including convFull"
+include("convFull.jl")
 
-    @info "including convFuzz"
-    include("convFuzz.jl")
+@info "including dense"
+include("dense.jl")
 
+@info "including denseFuzz"
+include("denseFuzz.jl")
 
+@info "including denseAdpt"
+include("denseAdpt.jl")
 
-    @info "including convAdpt"
-    include("convAdpt.jl")
-
-
-
-    @info "including convFull"
-    include("convFull.jl")
-
-
-
-    @info "including dense"
-    include("dense.jl")
-
-
-
-    @info "including denseFuzz"
-    include("denseFuzz.jl")
-
-
-
-    @info "including denseAdpt"
-    include("denseAdpt.jl")
-
-
-
-    @info "including denseFull"
-    include("denseFull.jl")
+@info "including denseFull"
+include("denseFull.jl")
